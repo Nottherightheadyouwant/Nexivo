607439cd3d0910d7a7c307c5f6d95dc2766eabda
Saurav19304 <sauravvaghela496@gmail.com>
2026-09-15 23:30:01 +0100

    Update Jay Parmar founder photo to new high-res cherry blossom portrait

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -34,7 +34,7 @@ export default function About() {
                 <img 
                   src={jayParmarImg} 
                   alt="Jay Parmar - Founder & Creative Director at Nexivo" 
-                  style={{ width: '100%', height: '440px', objectFit: 'cover', objectPosition: 'center 75%', display: 'block' }}
+                  style={{ width: '100%', height: '440px', objectFit: 'cover', objectPosition: '35% 55%', display: 'block' }}
                 />
               </div>
               <div style={{
